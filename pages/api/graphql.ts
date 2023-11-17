ba335816fa3a6df92a971552ff5593f13f5f4ac5
josuerushanika <josuerushanika@gmail.com>
2023-11-18 01:19:43 +0200

    Fix some isuues

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,23 +1,29 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-micro";
+import Cors from 'micro-cors';
 import { typeDefs } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
-import Cors from 'micro-cors';
 
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers});
-
+// Ensure that apolloServer.start() is called once
 const startServer = apolloServer.start();
 
-export default Cors( async function handler(req, res) {
-    await startServer;
+const cors = Cors(); // Apply Cors middleware here
 
-    await apolloServer.createHandler ({
-        path: '/api/graphql',
-    }) (req, res); 
+export default cors(async function (req, res) {
+  await startServer;
+
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return;
+  }
+
+  return apolloServer.createHandler({ path: '/api/graphql' })(req, res);
 });
 
 export const config = {
-    api: {
-        bodyParser: false,
-    }
-}
+  api: {
+    bodyParser: false,
+  },
+};
+
