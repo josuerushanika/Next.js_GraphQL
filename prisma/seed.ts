cfc11588b594ce751111735cbff523ede3cb2118
josuerushanika <josuerushanika@gmail.com>
2023-11-17 04:54:02 +0200

    Update file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,32 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { links } from "../data/links";
-
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      email: "testemail@gmail.com",
-      role: "ADMIN",
-    },
-  });
+  try {
+    await prisma.user.create({
+      data: {
+        email: "testemail@gmail.com",
+        role: "ADMIN",
+      },
+    });
 
-  await prisma.link.createMany({
-    data: links,
-  });
+    // Assuming 'link' is the correct model and has 'url' and 'description' fields
+    await prisma.link.createMany({
+      data: links.map(link => ({
+        url: link.url,
+        description: link.description,
+      })),
+    });
+
+    console.log("Seed data inserted successfully.");
+  } catch (e) {
+    console.error("Error during seeding:", e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 main();
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-   .finally(async() => {
-     await prisma.$disconnect();
-   });
---
+++ zo.js
@@ -0,0 +1,2 @@
+//https://www.youtube.com/watch?v=hZgFAO4_t4Q&list=PLn2e1F9Rfr6k6MwzS-p9FGK1NDBxxwLPk
+// 9e Video
